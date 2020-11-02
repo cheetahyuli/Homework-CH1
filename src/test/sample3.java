bafe75937b8bc6b2ece19a088eec6432323e511d
cheetahyuli <cheetahyuli@gmail.com>
2020-11-02 13:58:39 +0800

    first

---
+++ src/test/sample3.java
@@ -0,0 +1,11 @@
+package test;
+
+public class sample3
+{
+	public static void main(String[] args)
+	{
+		System.out.println('A');
+		System.out.println("開始使用Java吧!");
+		System.out.println(123);
+	}
+}
